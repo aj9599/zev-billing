b691b8bf0232af56af62a5431d3fb7c1ea86e35b
Arno Jungen <arnojungen@outlook.com>
2025-10-30 23:39:02 +0100

    Translation Fixes

--- frontend/src/i18n/translations/en.ts
+++ frontend/src/i18n/translations/en.ts
@@ -890,7 +890,7 @@ export const en: Translations = {
 
   "logs.cpuLow": "Low usage",
   "logs.cpuModerate": "Moderate usage",
-  "logs.cpuHigh": "High usage!",
+  "logs.cpuHigh": "High usage",
   "logs.updatingSystem": 'Updating System',
   "logs.updateInProgress": 'Please wait while the system is being updated. This will take approximately 20 seconds.',
   "logs.doNotCloseWindow": 'Please do not close this window or navigate away.',
