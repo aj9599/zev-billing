ab19af415650c5345048811ec421bf8b69465120
Arno Jungen <arnojungen@outlook.com>
2025-11-24 21:29:34 +0100

    bugfix

--- backend/services/zaptec_collector.go
+++ backend/services/zaptec_collector.go
@@ -1241,16 +1241,3 @@ func (zc *ZaptecCollector) GetCompletedSession(chargerID int) (*CompletedSession
 func (zc *ZaptecCollector) MarkSessionProcessed(sessionID string) {
 	// No-op - sessions are automatically marked as processed
 }
-
-func formatDuration(d time.Duration) string {
-	hours := int(d.Hours())
-	minutes := int(d.Minutes()) % 60
-	seconds := int(d.Seconds()) % 60
-	
-	if hours > 0 {
-		return fmt.Sprintf("%dh %dm", hours, minutes)
-	} else if minutes > 0 {
-		return fmt.Sprintf("%dm %ds", minutes, seconds)
-	}
-	return fmt.Sprintf("%ds", seconds)
-}
