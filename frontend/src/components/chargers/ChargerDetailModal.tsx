adbbc10370a5006af1434c1b45bc4c614f445edc
Arno Jungen <arnojungen@outlook.com>
2026-02-11 08:47:16 +0100

    update

--- frontend/src/components/chargers/ChargerDetailModal.tsx
+++ frontend/src/components/chargers/ChargerDetailModal.tsx
@@ -122,7 +122,7 @@ export default function ChargerDetailModal({
             left: 0,
             right: 0,
             bottom: 0,
-            backgroundColor: 'rgba(0,0,0,0.5)',
+            backgroundColor: 'rgba(0,0,0,0.15)',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
